c33e0a4f22b0b4fdfa14ad2c5a95c5a2b85863eb
joaquin <juaco.enero9@gmail.com>
2025-11-25 10:56:43 -0300

    eliminar video

--- js/API/getVideosApi.js
+++ js/API/getVideosApi.js
@@ -3,21 +3,64 @@ async function cargarVideos() {
     const videos = await res.json();
 
     const container = document.getElementById("videos-container");
-    container.innerHTML = ""; // limpia por si recargas
+    container.innerHTML = "";
 
     videos.forEach((video) => {
-        const vid = document.createElement("video");
-        vid.width = 240;
-        vid.height = 240;
-        vid.controls = true;
+        const card = document.createElement("div");
+        card.className = "video-card";
 
-        const source = document.createElement("source");
-        source.src = video.url;
-        source.type = "video/mp4";
+        card.innerHTML = `
+            <video width="240" height="240" controls>
+                <source src="${video.url}" type="video/mp4">
+            </video>
 
-        vid.appendChild(source);
-        container.appendChild(vid);
+            <div class="actions">
+                <button class="btn-delete" data-id="${video._id}">
+                    <i class="fa-solid fa-trash-can"></i>
+                </button>
+
+                <select class="slide-number" data-id="${video.id}">
+                    <option value="1">Posicion 1</option>
+                    <option value="2">Posicion 2</option>
+                    <option value="3">Posicion 3</option>
+                    <option value="4">Posicion 4</option>
+                </select>
+            </div>
+        `;
+
+        container.appendChild(card);
+    });
+
+    agregarEventosDelete();
+}
+cargarVideos();
+
+
+function agregarEventosDelete() {
+    const botones = document.querySelectorAll(".btn-delete");
+
+    botones.forEach(btn => {
+        btn.addEventListener("click", async () => {
+            const id = btn.dataset.id;
+            console.log("Borrar video con ID:", id);
+            const confirmar = confirm("¿Seguro que deseas eliminar este video?");
+            if (!confirmar) return;
+
+            try {
+                const res = await fetch(`https://back-slider.onrender.com/borrar/${id}`, {
+                    method: "DELETE"
+                });
+
+                const data = await res.json();
+                console.log("Respuesta:", data);
+
+                // refrescar lista
+                cargarVideos();
+
+            } catch (error) {
+                console.error("Error eliminando:", error);
+                alert("❌ No se pudo borrar el video");
+            }
+        });
     });
 }
-
-cargarVideos();
